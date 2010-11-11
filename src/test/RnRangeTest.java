3852dab190bc621d829bb59e48860ce9eab7e3d0
Marco <devnull@localhost>
2010-11-11 15:10:46 +0100

    Uniform Gen tested. Added Graphs.

--- src/test/RnRangeTest.java
+++ src/test/RnRangeTest.java
@@ -1,24 +1,26 @@
 package test;
 
 import generators.RnLongRangeGenerator;
-import generators.RnDoubleRangeGenerator;
 
 public class RnRangeTest {
 
+	
+	private static Long nextValue;
+	
 	public static void main(String[] args) {
 		
-		RnDoubleRangeGenerator rngen2 = new RnDoubleRangeGenerator(2L, 78L, 101L);
-		
-		for(int i = 0; i<100; i++){
-			System.out.println(rngen2.getNextValue());
-		}
-		
-		System.out.println("Generatore altro tipo");
+		int[] testArray = new int[78];
 		
 		RnLongRangeGenerator rngen1 = new RnLongRangeGenerator(2L, 78L, 101L);
 		
-		for(int i = 0; i<100; i++){
-			System.out.println(rngen1.getNextValue());
+		for(int i = 0; i<1000000; i++){
+			nextValue = rngen1.getNextValue();
+			testArray[nextValue.intValue()]++;
+			//System.out.println(nextValue);
+		}
+		
+		for(int i = 0; i< testArray.length; i++){
+			System.out.println(testArray[i]);	
 		}
 	}
 
