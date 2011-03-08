9d80c560a383332aef3f334365ef3fc35b9a2ed9
ferrari.marco <devnull@localhost>
2011-03-08 11:31:53 +0100

    Fixed ExpGenTest

--- src/test/ExpGenTest.java
+++ src/test/ExpGenTest.java
@@ -1,43 +1,22 @@
 package test;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import io.FileOutput;
 import generators.ExponentialGenerator;
 
 public class ExpGenTest {
 
 	public static void main(String[] args) {
 	
-		Double mean = 0.5;
+		ExponentialGenerator exp = new ExponentialGenerator(1L, 0.5);
+		GeneratorTest genTest = new GeneratorTest();
 		
-		ExponentialGenerator exp = new ExponentialGenerator(1L, mean);
-		ArrayList<String> distrib = new ArrayList<String>();
-		ArrayList<String> tempi = new ArrayList<String>();
-		
-		Double time = 0.0;
-		Double result = 0.0;
-		
-		String timeString = "";
-		String resultString = "";
-		DecimalFormat df = new DecimalFormat("##.##########");
-
+		Double time;
 		
 		for(int i=0; i<1000000; i++){
 			time = exp.generateNextValue();
-			result = exp.getDensity(time);
-			
-			timeString = df.format(time);
-			resultString = df.format(result);
-			
-			distrib.add(resultString);
-			tempi.add(timeString);
+			genTest.elaborateNewValue(time, exp.getDensity(time));
 		}
 		
-		FileOutput.textFileWriter(distrib, "C:\\distrib.txt", true, false, true);
-		FileOutput.textFileWriter(tempi, "C:\\tempi.txt", true, false, true);
-
-
+		genTest.finalize("C:\\ExpGen");
 	}
 
 }
