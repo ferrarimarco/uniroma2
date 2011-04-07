42321636b497b920b716d70e4a33b0dd6d71ed9f
danielelaviola <danielelaviola@gmail.com>
2011-04-07 12:32:06 +0200

    Modifica Generatori

--- src/generators/IperEspGenerator.java
+++ src/generators/IperEspGenerator.java
@@ -4,8 +4,9 @@ import interfaces.Generator;
 
 public class IperEspGenerator implements Generator {
 
-	private ExponentialGenerator expGen1;
-	private ExponentialGenerator expGen2;
+	//private ExponentialGenerator expGen1;
+	//private ExponentialGenerator expGen2;
+	private ExponentialGenerator expGen;
 	private UniformDoubleGenerator uniGen;
 	private Double tempo1;
 	private Double tempo2;
@@ -13,11 +14,11 @@ public class IperEspGenerator implements Generator {
 	private Double mean;
 
 	public IperEspGenerator(Long seed1, Long seed2, Double mean, Double prob) {
-		tempo1 = mean / 2 * prob;
-		tempo2 = mean / 2 * (1 - prob);
-		
-		expGen1 = new ExponentialGenerator(seed1, tempo1);
-		expGen2 = new ExponentialGenerator(seed2, tempo2);
+		tempo1 = mean / (2 * prob);
+		tempo2 = mean / (2 * (1 - prob));
+		expGen = new ExponentialGenerator(seed1, 1.0);
+		//expGen1 = new ExponentialGenerator(seed1, tempo1);
+		//expGen2 = new ExponentialGenerator(seed2, tempo2);
 		uniGen = new UniformDoubleGenerator(seed1);
 		this.prob = prob;
 		this.mean = mean;
@@ -25,10 +26,10 @@ public class IperEspGenerator implements Generator {
 
 	@Override
 	public Double generateNextValue() {
-		if (uniGen.generateNextValue() < prob)
-			return expGen1.generateNextValue();
+		if (uniGen.generateNextValue() >= prob)
+			return expGen.generateNextValue()*tempo1;
 		else
-			return expGen2.generateNextValue();
+			return expGen.generateNextValue()*tempo2;
 	}
 
 	public Double getDensity(Double t) {
