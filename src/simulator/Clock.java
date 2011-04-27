2ee6606da413db0a9b852d064cf1316eb3b1f7fc
danielelaviola <danielelaviola@gmail.com>
2011-04-27 12:20:39 +0200

    Terminata Classe Clock

--- src/simulator/Clock.java
+++ src/simulator/Clock.java
@@ -1,5 +1,43 @@
 package simulator;
 
 public class Clock {
+	
+	private double wallTimeStart, wallTimeStop;
+	private double simTime;
+	
+	public Clock()
+	{
+		this.wallTimeStart = System.currentTimeMillis();
+		setSimTime(0.0);
+		
+	}
 
+
+	public void advance(double deltaT)
+	{
+	this.simTime += deltaT;	
+	}
+	
+	public double getSimTime()
+	{
+		return this.simTime;
+	}
+	
+	public void setSimTime(double simTime)
+	{
+		this.simTime = simTime;	
+	}
+	
+	public void stopTheClock()
+	{
+		this.wallTimeStop = System.currentTimeMillis();
+	}
+	
+	public double getWallTimeDuration()
+	{
+		stopTheClock();
+		return this.wallTimeStop - this.wallTimeStart;
+	}
+	
 }
+	
