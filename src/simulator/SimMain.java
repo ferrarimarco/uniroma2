80ff08d8ee7ce5412a42b95daaf29b55026c1866
ferrari.marco <devnull@localhost>
2011-05-12 17:17:14 +0200

    Test Main

---
+++ src/simulator/SimMain.java
@@ -0,0 +1,14 @@
+package simulator;
+
+public class SimMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Sequenziatore seq = new Sequenziatore(12);
+		seq.simula(10);
+
+	}
+
+}
