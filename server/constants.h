b807661b56d35fba85187e3980b28ac5702563f7
Marco Ferrari <devnull@localhost>
2011-10-07 16:53:46 +0200

    Added some more constants to constants.h to manage packet and buffers dimension.

--- server/constants.h
+++ server/constants.h
@@ -1,11 +1,14 @@
 #define SERV_PORT	5193
 #define BACKLOG		10
 #define MAXLINE		1024
-#define MAX_PK_SIZE 512
-#define MAX_PK_DATA_SIZE (MAX_PK_SIZE - 4)
-#define WIN_DIMENSION 50
+
+#define BUFFER_READ_FILE_DIMENSION 16384
+#define MAX_PK_DATA_SIZE 512
+
+#define WIN_DIMENSION 2
+
 #define LOSS_PROBABILITY 0
 
-#define SERVER_SHARE_PATH "/media/sf_reliable_udp/server/share/"
-//#define SERVER_SHARE_PATH "/home/marco/test_fopen/"
+#define SERVER_SHARE_PATH "/home/marco/share/"
+
 #define FILES_ONLY 1
