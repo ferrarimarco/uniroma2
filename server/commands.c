d97661e848ef14dfcfb08d8c238bd665f18d425a
Marco Ferrari <devnull@localhost>
2011-10-07 16:52:08 +0200

    Removed Time and ACK commands from commands.c. Not needed anymore.

--- server/commands.c
+++ server/commands.c
@@ -17,22 +17,15 @@ void com_get(char *buff, int *rec_data_amount, int sock_child, struct sockaddr_i
 void com_put();
 void com_list(int sock_child, struct sockaddr_in cli_addr);
 void com_time(int sock_child, struct sockaddr_in cli_addr);
-void com_ack(char *buff);
 
 int seleziona_comando(char *buff, int *rec_data_amount){
 	
-	//SPEZZARE I DATI DA INVIARE IN PIU PARTI SE TROPPI!
-
 	if(buff[0] == 'G') //GET
 		return 0;
 	else if(buff[0] == 'P') //PUT
 		return 1;
 	else if(buff[0] == 'L') //LIST
 		return 2;
-	else if(buff[0] == 'T')//TIME
-		return 3;
-	else if(buff[0] == 'A')//ACK
-		return 4;
 }
 
 void esegui_comando(char *buff, int *received_data_amount, int command, int sock_child, struct sockaddr_in cli_addr){
@@ -47,9 +40,6 @@ void esegui_comando(char *buff, int *received_data_amount, int command, int sock
 		case 2: 
 			com_list(sock_child, cli_addr);
 			break;
-		case 3:
-			com_time(sock_child, cli_addr);
-			break;
     };
 }
 
@@ -62,9 +52,6 @@ void com_get(char *buff, int *rec_data_amount, int sock_child, struct sockaddr_i
 
 	int i;
 
-	//char *path = "/media/sf_reliable_udp/server/share/pac03.nfo\0";
-
-
 	//Controllo se il comando è ben formato
 	if(buff[0] == 'G' && buff[1] == 'E' && buff[2] == 'T' && buff[3] == ' '){
 
@@ -76,35 +63,17 @@ void com_get(char *buff, int *rec_data_amount, int sock_child, struct sockaddr_i
 		sprintf(path, "%s%s", SERVER_SHARE_PATH, file_name);
 		strcat(path, "\0");
 		
-		printf("PATH: %s\n", path);
-		
-		sender_window(path, sock_child, cli_addr);
+		// CONTROLLARE SE IL FILE ESISTE!
 
+		send_data(path, sock_child, cli_addr);
+		
 	}else{
 		printf("comando mal formato!");
-		//INVIARE PACCHETTO CONTENENTE STRINGA VUOTA (CONDIZIONE DI ERRORE)	
+		//INVIARE PACCHETTO CONTENENTE ERRORE
 	}
 }
 
 void com_put(){}
-
-void com_time(int sock_child, struct sockaddr_in cli_addr){
-	
-	char buff_send[MAXLINE];
-
-	//Test con time
-	time_t ticks;
-	ticks = time(NULL);
-    
-	/* scrive in buff l'orario nel formato ottenuto da ctime; snprintf impedisce l'overflow del buffer. */
-    snprintf(buff_send, sizeof(buff_send), "%.24s\r\n", ctime(&ticks)); /* ctime trasforma la data e l’ora da binario in ASCII. \r\n per carriage return e line feed*/
-
-	/* scrive sul socket il contenuto di buff */
-    if (sendto(sock_child, buff_send, strlen(buff_send), 0, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
-      perror("errore in sendto");
-      exit(-1);
-    }
-}
 
 void com_list(int sock_child, struct sockaddr_in cli_addr){
 
