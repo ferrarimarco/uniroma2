d56d1600f02a29c0a0410bb57b72be89bbc365a3
ferrari.marco <devnull@localhost>
2011-10-11 11:24:07 +0200

    Packet.h: more info on the status field in the comment just before that field.

--- server/Packet.h
+++ server/Packet.h
@@ -3,10 +3,17 @@
 
 typedef struct {
 	
+	// -2 = FIN
 	int seq_number;
 	
-	// -1 = in buffer/window but empty, 0 = not in window (buffered only), 1 = not transmitted but in window, 2 = transmitted, 3 = transmitted and ACKed
+	// -1 = in buffer/window but empty, 0 = not in window (buffered only), 1 = not transmitted but in window, 2 = transmitted/received, 3 = transmitted/received and ACKed
 	int status;
+	
+	// Position in the timeout queue
+	int position_timeout_queue;
+	
+	// To know how much data to read in the data array
+	int data_size;
 	
 	// To hold data
 	unsigned char data[MAX_PK_DATA_SIZE];
