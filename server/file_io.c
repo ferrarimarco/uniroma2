e8857ae18366d70e5a7b09f9f4097c18d7072281
ferrari.marco <devnull@localhost>
2011-10-14 15:56:49 +0200

    file_io.c: removed operation on strings.

--- server/file_io.c
+++ server/file_io.c
@@ -92,17 +92,14 @@ int file_size(char *path){
 size_t leggi_file(FILE *file, unsigned char **buff){
 	
 	*buff = (char *) malloc(MAX_PK_DATA_SIZE);
-	
-	//size_t i = fread(*buff, sizeof(char), MAX_PK_DATA_SIZE - 1, file);
 	size_t i = fread(*buff, sizeof(char), MAX_PK_DATA_SIZE, file);
-	//strcat(*buff, "\0");
 	
 	return i;
 }
 
 size_t scrivi_file(FILE *file, unsigned char *buff){
 	
-	size_t i = fwrite(buff, sizeof(char), MAX_PK_DATA_SIZE - 1, file);
+	size_t i = fwrite(buff, sizeof(char), MAX_PK_DATA_SIZE, file);
 	
 	return i;
 }
