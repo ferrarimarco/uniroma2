4680e396e260e82755dbe79e35f8b7980239dbd8
ferrari.marco <devnull@localhost>
2011-10-25 12:17:17 +0200

    constants.h: some clean up.

--- constants.h
+++ constants.h
@@ -1,11 +1,9 @@
 #define SERV_PORT	5193
-#define BACKLOG		10
 #define MAXLINE		1024
 
-//#define BUFFER_FILE_DIMENSION 16384
 #define MAX_PK_DATA_SIZE 512
 
-#define BUFFER_FILE_DIMENSION 5
+#define BUFFER_FILE_DIMENSION 16834
 #define WIN_DIMENSION 2
 
 // Timeout in msec
