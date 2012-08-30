992c83a329c7e8110cf7f0e8b253ea84c7d304ac
Marco Ferrari <ferrari.marco@gmail.com>
2012-08-30 16:03:06 +0200

    Minor formatting editing.

--- src/controller/persistance/StorageManager.java
+++ src/controller/persistance/StorageManager.java
@@ -1,43 +1,43 @@
 package controller.persistance;
 
-public class StorageManager implements PersistanceManager{
-
+public class StorageManager implements PersistanceManager {
+	
 	@Override
 	public void create(StorageLocation location, String id, String value) {
 		// TODO: handle creation if the record is already there
-		
+
 		PersistanceManager awsDynamoDBStorageManager = getAWSDynamoDBStorageManager();
-		
+
 		awsDynamoDBStorageManager.create(location, id, value);
 	}
 
 	@Override
 	public String read(StorageLocation location, String id) {
 		PersistanceManager awsDynamoDBStorageManager = getAWSDynamoDBStorageManager();
-		
+
 		// TODO: if not found return an empty ("") string
-		
+
 		return awsDynamoDBStorageManager.read(location, id);
 	}
 
 	@Override
 	public void update(StorageLocation location, String id, String value) {
-		
+
 		// TODO: handle creation if the record does not exists
-		
+
 		PersistanceManager awsDynamoDBStorageManager = getAWSDynamoDBStorageManager();
-		
+
 		awsDynamoDBStorageManager.update(location, id, value);
 	}
 
 	@Override
 	public void delete(StorageLocation location, String id) {
 		PersistanceManager awsDynamoDBStorageManager = getAWSDynamoDBStorageManager();
-		
+
 		awsDynamoDBStorageManager.delete(location, id);
 	}
-	
-	private AWSDynamoDBStorageManager getAWSDynamoDBStorageManager(){
+
+	private AWSDynamoDBStorageManager getAWSDynamoDBStorageManager() {
 		return StorageManagerFactory.createAWSDynamoDBStorageManager();
 	}
 
@@ -45,13 +45,12 @@ public class StorageManager implements PersistanceManager{
 	public boolean isPresent(StorageLocation location, String id) {
 
 		String result = read(location, id);
-		
-		if(result.isEmpty()){
+
+		if (result.isEmpty()) {
 			return false;
-		}else{
+		} else {
 			return true;
 		}
 	}
-	
-	
+
 }
