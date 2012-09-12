95c8c955497b6350b5cc49fe0a54e5fbfa9fa044
Marco Ferrari <ferrari.marco@gmail.com>
2012-09-12 13:24:53 +0200

    Moved common CommunicationHandler code to AbstractCommunicationHandler.

--- src/controller/AbstractCommunicationHandler.java
+++ src/controller/AbstractCommunicationHandler.java
@@ -1,14 +1,61 @@
 package controller;
 
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public abstract class AbstractCommunicationHandler implements CommunicationHandler {
 
-	protected static final String endline = "\r\n";
+	private static final String endline = "\r\n";
+	private static final String terminationOctet = ".";
+	
+	// 512 - 2 (for endline)
+	private final int maxCharsPerLinePOP3 = 510;
+	
+	private void sendLine(BufferedOutputStream writer, String msg, boolean multiLine, boolean lastLine) {
+		
+		try {
+			
+			msg += endline;
+			
+			System.out.println("Server invia:" + msg);
+			
+			// Check if the line starts with the termination octet.
+			if(multiLine && msg.startsWith(terminationOctet)){
+				
+				// Byte-stuff according to POP3 protocol specification
+				msg = terminationOctet + msg;
+			}			
+			
+			writer.write(msg.getBytes());
+
+			/* DEBUG: PRINT EXA CHAR VALUES
+			for(int i = 0; i < msg.getBytes().length; i++){
+				System.out.println(String.format("0x%02X", msg.getBytes()[i]));
+			}
+			*/
+			
+			if(multiLine && lastLine){
+				writer.write((terminationOctet + endline).getBytes());
+				
+				System.out.println("Server invia il carattere di terminazione.");
+			}
+			
+			writer.flush();
+			
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
 	
 	@Override
-	public abstract void sendResponse(BufferedOutputStream writer, String statusIndicator, String response);
+	public void sendResponse(BufferedOutputStream writer, String statusIndicator, String response){
+		if (response.length() > 0) {
+			sendLine(writer, statusIndicator + " " + response, false, false);
+		}else{
+			sendLine(writer, statusIndicator, false, false);
+		}
+	}
 
 	@Override
 	public void sendString(BufferedOutputStream writer, String string){
@@ -16,12 +63,54 @@ public abstract class AbstractCommunicationHandler implements CommunicationHandl
 	}
 	
 	@Override
-	public abstract void sendListAsMultiLineResponse(BufferedOutputStream writer, List<String> list);
-
+	public void sendBlankLineMultilineEnd(BufferedOutputStream writer){
+		sendLine(writer, "", true, true);
+	}
+	
 	@Override
-	public abstract void sendBlankLineMultilineEnd(BufferedOutputStream writer);
-
+	public void sendStringWithLinesLimit(BufferedOutputStream writer, String string, int linesLimit){
+		
+		if(string.length() <= maxCharsPerLinePOP3){// String fits one line
+			sendLine(writer, string, true, true);
+		}else{// Need more than one line
+			
+			// Compute how many lines are needed
+			int lines = 0;
+			
+			lines = string.length() / (maxCharsPerLinePOP3);
+			if(string.length() % (maxCharsPerLinePOP3) > 0){
+				lines++;
+			}
+			
+			// Check if the message fits in the lines limit specified
+			if(linesLimit < lines){
+				lines = linesLimit;
+			}
+			
+			for(int i = 0; i < lines; i++){
+				// TODO: check this count!
+				if(i == lines - 2){
+					sendLine(writer, string.substring(i * maxCharsPerLinePOP3, i * maxCharsPerLinePOP3 + maxCharsPerLinePOP3), true, false);
+				}else{
+					sendLine(writer, string.substring(i * maxCharsPerLinePOP3, string.length()), true, true);
+				}
+			}
+		}
+	}
+	
 	@Override
-	public abstract void sendStringWithLinesLimit(BufferedOutputStream writer, String string, int linesLimit);
+	public void sendListAsMultiLineResponse(BufferedOutputStream writer, List<String> list){
+		
+		if(list.size() > 0){
+			for(int i = 0; i < list.size(); i++){
+				// Check if sending the last line
+				if(i < list.size() - 1){
+					sendLine(writer, list.get(i), true, false);
+				}else{// Send last line
+					sendLine(writer, list.get(i), true, true);
+				}
+			}
+		}
+	}
 
 }
--- src/controller/smtp/SMTPCommunicationHandler.java
+++ src/controller/smtp/SMTPCommunicationHandler.java
@@ -1,63 +1,7 @@
 package controller.smtp;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.util.List;
-
 import controller.AbstractCommunicationHandler;
 
 public class SMTPCommunicationHandler extends AbstractCommunicationHandler {
-
-	public void sendLine(BufferedOutputStream writer, String msg) {
-		
-		try {
-			
-			msg += endline;			
-			
-			writer.write(msg.getBytes());
-			writer.flush();
-			
-			System.out.println("SMTP server invia:" + msg);
-			
-			/* DEBUG: PRINT EXA CHAR VALUES
-			for(int i = 0; i < msg.getBytes().length; i++){
-				System.out.println(String.format("0x%02X", msg.getBytes()[i]));
-			}
-			*/
-			
-		} catch (IOException ioException) {
-			ioException.printStackTrace();
-		}
-	}
-
-	@Override
-	public void sendResponse(BufferedOutputStream writer, String statusIndicator, String response) {
-		
-		sendLine(writer, statusIndicator + " " + response);
-		
-	}
-
-	@Override
-	public void sendString(BufferedOutputStream writer, String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void sendListAsMultiLineResponse(BufferedOutputStream writer, List<String> list){
-
-	}
-
-	@Override
-	public void sendBlankLineMultilineEnd(BufferedOutputStream writer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void sendStringWithLinesLimit(BufferedOutputStream writer, String string, int linesLimit) {
-		// TODO Auto-generated method stub
-		
-	}
 	
 }
