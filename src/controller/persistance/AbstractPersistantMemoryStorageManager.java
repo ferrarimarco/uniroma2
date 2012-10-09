2d9908acb35aafa165dec61611de55ad2eb3cbd5
Marco Ferrari <ferrari.marco@gmail.com>
2012-10-09 11:29:01 +0200

    Removed abstract methods from abstract storage manger classes because of unnecessary redundancy.

--- src/controller/persistance/AbstractPersistantMemoryStorageManager.java
+++ src/controller/persistance/AbstractPersistantMemoryStorageManager.java
@@ -1,35 +1,5 @@
 package controller.persistance;
 
-import java.util.List;
-
-
 public abstract class AbstractPersistantMemoryStorageManager implements PersistanceManager {
-
-	@Override
-	public abstract void create(StorageLocation location, List<FieldName> fieldNames, String... values);
-
-	@Override
-	public abstract String read(StorageLocation location, FieldName fieldName, String keyValue);
-
-	@Override
-	public abstract List<String> getSet(StorageLocation location, FieldName fieldName, String keyValue);
-
-	@Override
-	public abstract void update(StorageLocation location, String keyValue, List<FieldName> fieldNames, String... values);
-
-	@Override
-	public abstract void addToSet(StorageLocation location, String keyValue, FieldName fieldName, String... values);
-
-	@Override
-	public abstract void delete(StorageLocation location, String keyValue);
-
-	@Override
-	public abstract boolean isPresent(StorageLocation location, FieldName fieldName, String keyValue);
-
-	@Override
-	public abstract List<String> scanForMessageDimensions(String clientId, String userName);
-
-	@Override
-	public abstract List<String> getMessageUIDs(StorageLocation location, String clientId, String userName, boolean isToDelete);
 
 }
