7c84a2f6f5cb9e7a4f6f67d8f0d1fccb7122104d
Marco Ferrari <ferrari.marco@gmail.com>
2013-01-29 23:35:23 +0100

    Working on the StatusMatrixGenerator class.

--- mpsr/src/it/uniroma2/disp/mpsr/Simulator.java
+++ mpsr/src/it/uniroma2/disp/mpsr/Simulator.java
@@ -1,17 +1,12 @@
 package it.uniroma2.disp.mpsr;
 
-import info.ferrarimarco.java.helper.math.SimpleMatrix;
-
 public class Simulator {
 
 	private static final int nodes = 4;
 	private static final int users = 35;
 	
 	public static void main(String[] args) {
-
-		SimpleMatrix<Integer> statusMatrix = StatusMatrixGenerator.generateStatus(nodes, users);
-		
-		System.out.println(statusMatrix.toString());		
+	
 	}
 
 }
--- mpsr/src/it/uniroma2/disp/mpsr/StatusMatrixGenerator.java
+++ mpsr/src/it/uniroma2/disp/mpsr/StatusMatrixGenerator.java
@@ -1,64 +1,80 @@
 package it.uniroma2.disp.mpsr;
 
-import info.ferrarimarco.java.helper.math.DigitsHelper;
-import info.ferrarimarco.java.helper.math.SimpleMatrix;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
+import info.ferrarimarco.java.helper.math.CombinatoricsHelper;
+import info.ferrarimarco.java.helper.math.SimpleMatrix;
 
 public class StatusMatrixGenerator {
 
-	public static SimpleMatrix<Integer> generateStatus(int nodes, int users){
+	private SimpleMatrix<Integer> statusMatrix;
+	private int users;
+	private int rows;
+	
+	public StatusMatrixGenerator(int users, int nodes){
 		
-		List<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+		rows = CombinatoricsHelper.binomialCoefficient(users + nodes - 1, nodes - 1);
 		
-		int powArgument = nodes - 1;
+		statusMatrix = new SimpleMatrix<Integer>(rows, nodes, 0);
 		
-		int maximum = (int) (users * Math.pow(10, powArgument));
+		this.users = users;
 		
-		List<Integer> numbersToWrite = new ArrayList<Integer>(maximum);
-		
-		for(int i = 1; i < maximum + 1; i++){
-			
-			int sum = DigitsHelper.computeDigitsSum(i);
-			
-			if(sum == users){
-				numbersToWrite.add(i);
-			}		
-		}
-		
-		Collections.sort(numbersToWrite, Collections.reverseOrder());
-		
-		for(int i = 0; i < numbersToWrite.size(); i++){
-			
-			ArrayList<Integer> row = new ArrayList<Integer>(nodes);
-			
-			for(int j = 0; j < nodes; j++){
-				row.add(0);
-			}
-			
-			int num = numbersToWrite.get(i);
-			int zeroes = row.size() - DigitsHelper.getNumberLenght(num);
-			
-			for(int j = zeroes; j < row.size(); j++){
-				row.set(j, DigitsHelper.getIthDigit(j + 1 - zeroes, num));
-			}
-			
-			matrix.add(row);
-		}
-		
-		SimpleMatrix<Integer> simpleMatrix = new SimpleMatrix<Integer>(matrix.size(), matrix.get(0).size(), 0);
-		
-		for(int i = 0; i < matrix.size(); i++){
-			for(int j = 0; j < matrix.get(i).size(); j++){
-				simpleMatrix.setElement(i, j, matrix.get(i).get(j));
-			}
-		}
-		
-		return simpleMatrix;
 	}
 	
+	public void generateStatusMatrix(){
+		System.out.println(generateStatus(0, users, statusMatrix.getColumns()).toString());
+	}
+	
+	public ArrayList<ArrayList<Integer>> generateStatus(int numStati, int numCentri, int numJob){
+		
+		int i, j, k, numSubStati;
+		
+		ArrayList<ArrayList<Integer>> stati_generati = new ArrayList<ArrayList<Integer>>(numCentri);
+		
+		
+		for(int h = 0;h < numCentri;h++){
+			stati_generati.add(new ArrayList<Integer>());
+		}
+		
+		
+		if(numCentri==1){
+			
+			stati_generati.get(0).add(numJob);
+		}
+		else if(numCentri==2){
+			
+			for(i = 0;i < numStati; i++){
+				stati_generati.get(0).add(numJob-i);
+				stati_generati.get(1).add(i);
+			}
+		}
+		else if(numCentri>=3){
+			
+			for(j=0; j<numCentri; j++){
+				if(j==0) stati_generati.get(j).add(numJob);
+				else stati_generati.get(j).add(0);
+			}
+			
+			for(i=1; i<=numJob; i++){
+				numSubStati = calcola_card_E(i, numCentri - 1);
+				ArrayList<ArrayList<Integer>> tmp_stati_generati = new ArrayList<ArrayList<Integer>>(numCentri-1);
+				tmp_stati_generati = this.generateStatus(numSubStati, numCentri-1, i);
 
+				for(j=1; j<numSubStati+1; j++){
+					stati_generati.get(0).add(numJob-i);
+					for(k=1; k<numCentri; k++){
+						stati_generati.get(k).add(tmp_stati_generati.get(k-1).get(j-1));
+					}
+				}
+			}
+		}
+		
+		return stati_generati;
+	}
+	
+	public int calcola_card_E(int users, int nodes){
+		return CombinatoricsHelper.binomialCoefficient(users + nodes -1, nodes - 1);
+	}
+	
+	
 }
