2f9ac23c4520491e30ccfca019bd9094db76c30b
Marco Ferrari <ferrari.marco@gmail.com>
2013-02-08 22:41:32 +0100

    Simulator update to compute saturation probability.

--- mpsr/src/it/uniroma2/disp/mpsr/Simulator.java
+++ mpsr/src/it/uniroma2/disp/mpsr/Simulator.java
@@ -1,54 +1,75 @@
 package it.uniroma2.disp.mpsr;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import info.ferrarimarco.java.helper.math.SimpleMatrix;
+import it.uniroma2.disp.mpsr.helper.GNCalculator;
+import it.uniroma2.disp.mpsr.helper.ProbabilityHelper;
+import it.uniroma2.disp.mpsr.helper.SaturationHelper;
+import it.uniroma2.disp.mpsr.init.MuGenerator;
+import it.uniroma2.disp.mpsr.init.NodesInitializer;
+import it.uniroma2.disp.mpsr.init.RoutingMatrixGenerator;
+import it.uniroma2.disp.mpsr.init.StatusMatrixGenerator;
+import it.uniroma2.disp.mpsr.init.XCalculator;
+import it.uniroma2.disp.mpsr.model.Node;
 
 public class Simulator {
 
 	private static final int nodes = 4;
-	private static final int users = 2;
+	private static final int users = 35;
 	
 	public static void main(String[] args) {
 		
-		double mu0 = 1.0/7.0; // client1
-		double mu1 = 1/0.3; // FE server
-		double mu2 = 1/0.08; // BE server
-		double mu3 = 1.0/7.0; // client2
-		
-		System.out.println("mu0:" + mu0);
-		System.out.println("mu1:" + mu1);
-		System.out.println("mu2:" + mu2);
-		System.out.println("mu3:" + mu3);
-		
+		double[] mu = MuGenerator.computeMus(nodes);
+
 		StatusMatrixGenerator statusMatrixGenerator = new StatusMatrixGenerator(users, nodes);
-		
 		SimpleMatrix<Integer> statusMatrix = statusMatrixGenerator.generateStatusMatrix();
-		SimpleMatrix<Double> routingMatrix = RoutingMatrixGenerator.generateRoutingMatrix(4);
 		
-		System.out.println(statusMatrix.toString());
-		System.out.println(routingMatrix.toString());
+		SimpleMatrix<Double> routingMatrix = RoutingMatrixGenerator.generateRoutingMatrix(nodes);
 		
-		double x0 = 5.0;
-		double x1 = ((5.0 * mu0) / mu1) * (1.0 + routingMatrix.getElement(2, 3)/routingMatrix.getElement(2, 0));
-		double x2 = (5.0 * mu0) / (mu2 * routingMatrix.getElement(2, 0));
-		double x3 = (5.0 * mu0 * routingMatrix.getElement(2, 3)) / (mu3 * routingMatrix.getElement(2, 0));
+		double[] x = XCalculator.computeX(nodes, routingMatrix, mu);
 		
-		System.out.println("x0:" + x0);
-		System.out.println("x1:" + x1);
-		System.out.println("x2:" + x2);
-		System.out.println("x3:" + x3);
-		
-		double GN = 0.0;
-		
-		for(int i = 0; i < statusMatrix.getRows(); i++){
-			double GNTerm = Math.pow(x0, statusMatrix.getElement(i, 0))
-					* Math.pow(x1, statusMatrix.getElement(i, 1))
-					* Math.pow(x2, statusMatrix.getElement(i, 2))
-					* Math.pow(x3, statusMatrix.getElement(i, 3));
-			GN += GNTerm;
+		for(int i = 0; i < mu.length; i++){
+			System.out.println("mu_" + i + " = " + mu[i]);
 		}
 		
+		for(int i = 0; i < x.length; i++){
+			System.out.println("x_" + i + " = " + x[i]);
+		}
 		
+		List<Node> nodesList = NodesInitializer.generateNodes(nodes);
+		
+		double GN = GNCalculator.computeGN(statusMatrix, x, nodesList);
 		System.out.println("G(N): " + GN);
+		
+		double GN_1 = 1.0/GN;
+		System.out.println("1/G(N): " + GN_1);
+		
+		int threshold = 1;
+		
+		Map<Integer,Double> saturationMap = new HashMap<Integer,Double>();
+		
+		// prob che nfe + nbe > soglia
+		while(threshold <= users){
+			
+			double sum = 0;
+			
+			List<Integer> saturatedStatusesIndexes = SaturationHelper.saturatedStatusesFinder(threshold, statusMatrix, nodesList);
+			
+			for(int i = 0; i < saturatedStatusesIndexes.size(); i++){
+				
+				List<Integer> status = statusMatrix.getRow(saturatedStatusesIndexes.get(i));
+				
+				sum += ProbabilityHelper.computeStatusProbability(status, nodesList, x, GN);
+			}
+			
+			saturationMap.put(threshold, sum);
+			
+			System.out.println(threshold + " " + sum);
+			
+			threshold++;
+		}
 	}
-
 }
