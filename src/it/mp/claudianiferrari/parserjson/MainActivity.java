58de8bf40a576525763db227806d4d1298f0eb18
Marco Ferrari <ferrari.marco@gmail.com>
2013-05-08 14:50:21 +0200

    Removed an unused setter from MainActivity

--- src/it/mp/claudianiferrari/parserjson/MainActivity.java
+++ src/it/mp/claudianiferrari/parserjson/MainActivity.java
@@ -1,12 +1,7 @@
 package it.mp.claudianiferrari.parserjson;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 
 public class MainActivity extends Activity {
 
@@ -19,12 +14,5 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		new DownloadJsonObjectTask(this).execute(url);
-	}
-	
-	public void setContatti(List<String> contactsList){
-		
-		ListView listView = (ListView) findViewById(R.id.ListViewContatti);
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsList);
-		listView.setAdapter(arrayAdapter);
 	}
 }
