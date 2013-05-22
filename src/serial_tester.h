e9a2db61baca3650153158414c87e5b9988bb08c
Marco Ferrari <ferrari.marco@gmail.com>
2013-05-22 18:03:39 +0200

    First commit (serial tester already completed).

---
+++ src/serial_tester.h
@@ -0,0 +1,15 @@
+/*
+ * serial_tester.h
+ *
+ *  Created on: 21/mag/2013
+ *      Author: Marco
+ */
+
+#ifndef SERIAL_TESTER_H_
+#define SERIAL_TESTER_H_
+
+int serial_test(double numbers[], int number_size, int k_subintervals, double alpha);
+
+#endif /* SERIAL_TESTER_H_ */
+
+
