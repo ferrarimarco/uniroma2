5df42a0b26936ca8d9c0702b63f753c98d9447ed
Marco Ferrari <ferrari.marco@gmail.com>
2013-05-25 16:42:44 +0200

    implemented two (linear) transformations: pareto and weibull.

---
+++ src/transformations.c
@@ -0,0 +1,37 @@
+/*
+ * transformations.c
+ *
+ *  Created on: 25/mag/2013
+ *      Author: Marco
+ */
+
+#include <math.h>
+#include <stdlib.h>
+
+#include "transformations.h"
+
+double pareto(double u, double k, double alpha){
+
+	double num = k;
+
+	double pow_arg = 1.0 / alpha;
+	double den = pow(u, pow_arg);
+
+	double result = num / den;
+
+	return result;
+}
+
+double weibull(double u, double a, double b){
+
+	double first_term = a;
+
+	double second_term_arg = log(u);
+	double exp = 1.0 / b;
+	double second_term = pow(1.0 - log(u), exp);
+
+	double result = first_term * second_term;
+
+	return result;
+}
+
---
+++ src/transformations.h
@@ -0,0 +1,16 @@
+/*
+ * transformations.h
+ *
+ *  Created on: 25/mag/2013
+ *  Author: Marco Ferrari <ferrari.marco@gmail.com>
+ *  Website: http://www.ferrarimarco.info
+ */
+
+#ifndef TRANSFORMATIONS_H_
+#define TRANSFORMATIONS_H_
+
+double pareto(double u, double k, double alpha);
+
+double weibull(double u, double a, double b);
+
+#endif /* TRANSFORMATIONS_H_ */
