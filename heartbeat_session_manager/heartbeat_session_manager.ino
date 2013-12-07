dfb59e2fc809e877f58a964135512e0f242f102c
Marco Ferrari <ferrari.marco@gmail.com>
2013-12-07 11:53:52 +0100

    Lots of work. In short: communication with the Spring MVC servlet (via HTTP) is working.

--- heartbeat_session_manager/heartbeat_session_manager.ino
+++ heartbeat_session_manager/heartbeat_session_manager.ino
@@ -6,7 +6,7 @@
 #define NELEMS(x)  (sizeof(x) / sizeof(x[0]))
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x77, 0x25 };
-char server[] = "192.168.0.13";
+char server[] = "192.168.0.11";
 EthernetClient client;
 
 AES aes;
@@ -28,18 +28,26 @@ byte plain[] =
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 32 to 63 = SHA-256 hash of the previous fields
 };
 
-byte session_id[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };
-
-byte padding [] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };
-
-byte hash [] = {
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-
 byte cipher [4 * N_BLOCK];
 byte check [4 * N_BLOCK];
 
+int BLOCK_LEN_BYTE = 64;
+
+
+void print_plain_block(){
+  for(int i = 0; i < BLOCK_LEN_BYTE; i++){
+    char c = (char) plain[i];
+    
+    Serial.print(c);
+  }
+  
+  Serial.println();
+}
+
 void init_eth_shield(){
+
+  Serial.println("ETH Shield init start");
+  
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -53,62 +61,68 @@ void init_eth_shield(){
   delay(1000);
 }
 
-void clear_plain(){
+void clear_plain(boolean clear_session_id){
   int plain_len = NELEMS(plain);
   
   for(int i = 0; i < plain_len; i++){
-    plain[i] = 0x00;
+    if(clear_session_id){
+      plain[i] = 0x00;
+    }else{
+      if(i < 3 || i > 13){
+        plain[i] = 0x00;
+      }
+    }
   }
-}
-
-void clear_session_id(){
-  int session_id_len = NELEMS(session_id);
-  
-  for(int i = 0; i < session_id_len; i++){
-    session_id[i] = 0x00;
-  }
-}
-
-void clear_padding(){
-  int len = NELEMS(padding);
-  
-  for(int i = 0; i < len; i++){
-    padding[i] = 0x00;
-  }
-}
-
-void clear_hash(){
-  int len = NELEMS(hash);
-  
-  for(int i = 0; i < len; i++){
-    hash[i] = 0x00;
-  }
-}
-
-void init_padding(){
-	clear_padding();
-	
-	// TODO: zero padding or random padding?
 }
 
 void init_session_id(){
   
-  clear_session_id();  
-  
   send_session_id_get_request();
   
-  // TODO: initialize the session ID array
+  String session_id = read_http_response_body();
+  
+  int session_id_len = session_id.length();
+  
+  Serial.print("Session ID Len: ");
+  Serial.println(session_id_len);
+  
+  for(int i = 0; i < session_id_len; i++){
+    plain[i + 3] = (byte) session_id.charAt(i);
+  }
+  
+  Serial.print("Plain block after session init: ");
+  print_plain_block();
+}
+
+void init_plain(byte seq_1, byte seq_2, byte seq_3, byte hb_value){
+  clear_plain(false);
+  
+  plain[0] = seq_1;
+  plain[1] = seq_2;
+  plain[2] = seq_3;
+  
+  plain[15] = hb_value;
+  
+  // Padding
+  for(int i = 16; i < 32; i++){
+    plain[i] = 0x00;
+  }
+  
+  // Hash
+  for(int i = 32; i < 64; i++){
+    plain[i] = 0x00;
+  }
 }
 
 void send_session_id_get_request(){
-  Serial.println("connecting...");
+  Serial.println("connecting to send GET (session_id) request...");
   
   // if you get a connection, report back via serial:
   if (client.connect(server, 8080)) {
     Serial.println("connected");
     // Make a HTTP request:
     client.println("GET /sii-heart-monitor/io/arduino/session_id/user/marco HTTP/1.1");
-    client.println("Host: 192.168.0.13");
+    client.println("Host: 192.168.0.11");
     client.println("Connection: close");
     client.println();
   } else {
@@ -117,77 +131,101 @@ void send_session_id_get_request(){
   }
 }
 
-void init_plain(byte seq_1, byte seq_2, byte seq_3, byte hb_value){
-  clear_plain();
+// for testing we use the plain block
+String cypher_block_to_string(){
+  String s = "";
   
-  plain[0] = seq_1;
-  plain[1] = seq_2;
-  plain[2] = seq_3;
-  
-  // Session ID
-  for(int i = 3; i < 15; i++){
-    plain[i] = session_id[i-3];
+  Serial.println("Start block processing");
+
+  for(int i = 0; i < BLOCK_LEN_BYTE; i++){
+    
+    char c = '0';
+    byte b = plain[i];
+    if(b != 0x00){
+      c = (char) plain[i];
+    }
+
+    s = s + String(c);
   }
   
-  plain[15] = hb_value;
+  Serial.println("Done block processing");
   
-  // Padding
-  for(int i = 16; i < 32; i++){
-    plain[i] = padding[i-16];
+  return s;
+}
+
+void send_hb_value_put_request(){
+  
+  Serial.println("connecting to send PUT request...");
+  
+  String put_request_argument = cypher_block_to_string();
+  
+  Serial.print("PUT request argument: ");
+  Serial.println(put_request_argument);
+  
+    // if you get a connection, report back via serial:
+  if (client.connect(server, 8080)) {
+    Serial.println("connected");
+    
+    String http_put_request = "PUT /sii-heart-monitor/io/arduino/session/store/";
+    http_put_request = http_put_request + put_request_argument + " HTTP/1.1";
+    
+    Serial.print("HTTP PUT request: ");
+    Serial.println(http_put_request);
+    
+    // Make a HTTP request
+    client.println(http_put_request);
+    client.println("Host: 192.168.0.11");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // kf you didn't get a connection to the server:
+    Serial.println("connection failed");
   }
   
-  // Hash
-  for(int i = 32; i < 64; i++){
-    plain[i] = hash[i-32];
-  }
+  read_http_response_body();
 }
 
 // bits = lunghezza key
 // blocks = 
 void send_hb_value(int bits, int blocks, byte hb_value, byte seq_num_1, byte seq_num_2, byte seq_num_3)
 {
-  long t0 = micros();
-  byte succ = aes.set_key(key, bits);
-  long t1 = micros() - t0;
+  //long t0 = micros();
+  //byte succ = aes.set_key(key, bits);
+  //long t1 = micros() - t0;
   
-  Serial.print("set_key ");
-  Serial.print(bits);
-  Serial.print(" ->");
-  Serial.print((int) succ);
-  Serial.print(" took ");
-  Serial.print(t1);
-  Serial.println("us");
+//  Serial.print("set_key ");
+//  Serial.print(bits);
+//  Serial.print(" ->");
+//  Serial.print((int) succ);
+//  Serial.print(" took ");
+//  Serial.print(t1);
+//  Serial.println("us");
+//  
+//  t0 = micros();
   
-  t0 = micros();
+  init_plain(seq_num_1, seq_num_2, seq_num_3, hb_value);
   
-  byte * padding;
-  byte * hash;
+//  succ = aes.encrypt(plain, cipher);
   
-  init_plain(seq_num_1, byte seq_num_2, byte seq_num_3, hb_value, padding, hash);
-  
-  plain[0] = 0xC0;
-  
-  succ = aes.encrypt(plain, cipher);
-  
-  t1 = micros() - t0;
+//  t1 = micros() - t0;
 
-  Serial.print("encrypt ");
-  Serial.print((int) succ);
-  Serial.print(" took ");
-  Serial.print(t1);
-  Serial.println("us");
-  
-  t0 = micros();
-
-  succ = aes.decrypt(cipher, plain);
-  
-  t1 = micros() - t0;
-  
-  Serial.print("decrypt ");
-  Serial.print((int) succ);
-  Serial.print(" took ");
-  Serial.print(t1);
-  Serial.println("us");
+//  Serial.print("encrypt ");
+//  Serial.print((int) succ);
+//  Serial.print(" took ");
+//  Serial.print(t1);
+//  Serial.println("us");
+//  
+//  t0 = micros();
+//
+//  succ = aes.decrypt(cipher, plain);
+//  
+//  t1 = micros() - t0;
+//  
+//  Serial.print("decrypt ");
+//  Serial.print((int) succ);
+//  Serial.print(" took ");
+//  Serial.print(t1);
+//  Serial.println("us");
 
   // ph == 0 stampa PLAIN (prima riga output)
   // ph == 1 stampa CYPHER (seconda riga output)
@@ -203,7 +241,59 @@ void send_hb_value(int bits, int blocks, byte hb_value, byte seq_num_1, byte seq
       Serial.print(val&15, HEX);
       Serial.print(" ");
     }
+    
     Serial.println();
+  }
+}
+
+String read_http_response_body(){
+  
+  while(client.connected() && !client.available()) delay(1);
+  
+  String response = "";
+  
+  char c;
+  
+  while (client.available()) {
+        c = client.read();
+        response = response + c;
+  }
+  
+  stop_eth_client();
+  
+  Serial.println("Response: ");
+  
+  Serial.println(response);
+  
+  int firstCr = 0;
+  int secondCr = 0;
+  
+  int response_len = response.length();
+  
+  while(secondCr != firstCr + 2){
+    firstCr = response.indexOf('\r', firstCr);
+    secondCr = response.indexOf('\r', firstCr + 1);
+    
+    if(secondCr != firstCr + 2){
+      firstCr = secondCr;
+    }
+ }
+  
+  int responseBodyStart = secondCr + 2;
+  String response_body = response.substring(responseBodyStart);
+  
+  Serial.print("Response body: ");
+  Serial.println(response_body);
+    
+  return response_body;
+}
+
+void stop_eth_client(){
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
   }
 }
 
@@ -212,44 +302,18 @@ void setup ()
   Serial.begin(57600);
   
   init_eth_shield();
-
-  init_session_id();
-  init_padding();
   
-  Serial.println("testing mode");
-
-  prekey(256, 1);
+  clear_plain(true);
+  
+  init_session_id();
+  
+  send_hb_value_put_request();
 }
 
 void loop () 
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.println("CHAR: ");
-    Serial.print(c);
-    Serial.print(" BYTE: ");
-    byte b = (byte) c;
-    
-    Serial.print(b);
-    
-    Serial.println(" CHAR (di nuovo): ");
-    
-    char c2 = (char) b;
-    
-    Serial.println(c2);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-
-    // do nothing forevermore:
-    while(true);
-  }
+  // do nothing forevermore:
+  while(true);
 
 }
 
