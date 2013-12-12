77aa5ad484a03c486522b10c9bab71dd731c13fb
Marco Ferrari <ferrari.marco@gmail.com>
2013-12-12 18:19:38 +0100

    - Added two exceptions: InvalidSequenceNumberException, SessionNotAvailableException

---
+++ src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/exceptions/InvalidSequenceNumberException.java
@@ -0,0 +1,11 @@
+package info.ferrarimarco.uniroma2.sii.heartmonitor.exceptions;
+
+
+public class InvalidSequenceNumberException extends Exception {
+
+	private static final long serialVersionUID = -7589245109226390328L;
+
+	public InvalidSequenceNumberException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/exceptions/SessionNotAvailableException.java
@@ -0,0 +1,12 @@
+package info.ferrarimarco.uniroma2.sii.heartmonitor.exceptions;
+
+
+public class SessionNotAvailableException extends Exception {
+
+	private static final long serialVersionUID = 2912903805979827117L;
+	
+	public SessionNotAvailableException(String msg) {
+		super(msg);
+	}
+
+}
