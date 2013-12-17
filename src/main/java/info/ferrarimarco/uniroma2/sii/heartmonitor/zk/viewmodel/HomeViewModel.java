d5dfc6d3e0bc279b8abff4bac1f0da6531d56a50
Marco Ferrari <ferrari.marco@gmail.com>
2013-12-18 00:39:37 +0100

    - Working on home.zul and associated viewmodel

--- src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/zk/viewmodel/HomeViewModel.java
+++ src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/zk/viewmodel/HomeViewModel.java
@@ -1,37 +1,47 @@
 package info.ferrarimarco.uniroma2.sii.heartmonitor.zk.viewmodel;
 
+import info.ferrarimarco.uniroma2.sii.heartmonitor.dao.repositories.CurrentHeartbeatSessionRepository;
+import info.ferrarimarco.uniroma2.sii.heartmonitor.model.CurrentHeartbeatSession;
 import info.ferrarimarco.uniroma2.sii.heartmonitor.model.HeartbeatSession;
-import info.ferrarimarco.uniroma2.sii.heartmonitor.model.HeartbeatSessionValue;
 import info.ferrarimarco.uniroma2.sii.heartmonitor.model.User;
 import info.ferrarimarco.uniroma2.sii.heartmonitor.services.SessionManagerService;
+import info.ferrarimarco.uniroma2.sii.heartmonitor.services.persistence.CurrentHeartbeatSessionPerisistenceService;
 import info.ferrarimarco.uniroma2.sii.heartmonitor.services.persistence.HeartbeatSessionPersistenceService;
 
 import java.util.List;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.ListModelList;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class HomeViewModel {
 	
-	private static Logger logger = LoggerFactory.getLogger(HomeViewModel.class);;
+	private static Logger logger = LoggerFactory.getLogger(HomeViewModel.class);
 	
 	@WireVariable
 	private HeartbeatSessionPersistenceService heartbeatSessionPersistenceService;
 	
 	@WireVariable
+	private CurrentHeartbeatSessionPerisistenceService currentHeartbeatSessionPerisistenceService;
+	
+	@WireVariable
 	private SessionManagerService sessionManagerService;
 	
 	private User currentUser;
+	private String currentStatus;
 	
 	private HeartbeatSession selectedHeartbeatSession;
 	
-	private List<HeartbeatSession> storedSessions;
+	private ListModelList<HeartbeatSession> storedSessions;
+	
+	public HomeViewModel() {
+		setCurrentStatus("Select a session or start a new session");
+	}
 	
 	@AfterCompose
 	public void afterCompose() {
@@ -40,13 +50,59 @@ public class HomeViewModel {
 		refreshHeartbeatSessionList();
 	}
 	
+	public HeartbeatSession getSelectedHeartbeatSession() {
+		return selectedHeartbeatSession;
+	}
+
+	
+	public void setSelectedHeartbeatSession(HeartbeatSession selectedHeartbeatSession) {
+		this.selectedHeartbeatSession = selectedHeartbeatSession;
+	}
+
+	
+	public ListModelList<HeartbeatSession> getStoredSessions() {
+		return storedSessions;
+	}
+
+	
+	public void setStoredSessions(ListModelList<HeartbeatSession> storedSessions) {
+		this.storedSessions = storedSessions;
+	}
+
 	private void refreshHeartbeatSessionList() {
-		storedSessions = heartbeatSessionPersistenceService.readHeartbeatSessions(currentUser.getUserName());
+		List<HeartbeatSession> sessionsForCurrentUser = heartbeatSessionPersistenceService.readHeartbeatSessions(currentUser.getUserName());
+		
+		storedSessions = new ListModelList<>(sessionsForCurrentUser);
 	}
 	
 	@Command
 	public void replaySelectedSession() {
 		
+	}
+	
+	@Command
+	public void startNewSession() {
+		HeartbeatSession session = new HeartbeatSession(currentUser.getUserName());
+		
+		session = heartbeatSessionPersistenceService.storeHeartbeatSession(session);
+		
+		currentHeartbeatSessionPerisistenceService.storeCurrentHeartbeatSession(new CurrentHeartbeatSession(currentUser.getUserName(), session.getId()));
+	}
+	
+	@Command
+	public void stopCurrentSession() {
+		
+		CurrentHeartbeatSession currentHeartbeatSession = currentHeartbeatSessionPerisistenceService.readCurrentHeartbeatSession(currentUser.getUserName());
+		
+		if(currentHeartbeatSession == null) { // the interface is replaying a session
+			
+		}else { // the interface is currently recording a new session
+			HeartbeatSession session = heartbeatSessionPersistenceService.readHeartbeatSession(currentHeartbeatSession.getCurrentSessionId());
+			session.setClosed(true);
+			
+			heartbeatSessionPersistenceService.storeHeartbeatSession(session);
+			currentHeartbeatSessionPerisistenceService.deleteCurrentHeartbeatSession(currentHeartbeatSession);
+		}
 	}
 	
 	@Command
@@ -70,5 +126,13 @@ public class HomeViewModel {
 	@Command
 	public void stopHeartbeatValuesFlowDisplay() {
 		
+	}
+
+	public String getCurrentStatus() {
+		return currentStatus;
+	}
+
+	public void setCurrentStatus(String currentStatus) {
+		this.currentStatus = currentStatus;
 	}
 }
