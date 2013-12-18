79a8c09d016c14c6737e3765b8a8c9265ef9062a
Marco Ferrari <ferrari.marco@gmail.com>
2013-12-18 17:47:08 +0100

    - CurrentHeartbeatSessionPerisistenceService: added a method to read all records; added a method to find the (only one that should be there) current session

--- src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/services/persistence/CurrentHeartbeatSessionPerisistenceService.java
+++ src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/services/persistence/CurrentHeartbeatSessionPerisistenceService.java
@@ -1,10 +1,17 @@
 package info.ferrarimarco.uniroma2.sii.heartmonitor.services.persistence;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import info.ferrarimarco.uniroma2.sii.heartmonitor.dao.repositories.CurrentHeartbeatSessionRepository;
 import info.ferrarimarco.uniroma2.sii.heartmonitor.model.CurrentHeartbeatSession;
 
 public class CurrentHeartbeatSessionPerisistenceService extends AbstractPersistenceService{
 
+	private Logger logger = LoggerFactory.getLogger(CurrentHeartbeatSessionPerisistenceService.class);
+	
 	private CurrentHeartbeatSessionRepository repository;
 	
 	public CurrentHeartbeatSessionPerisistenceService() {
@@ -33,8 +40,30 @@ public class CurrentHeartbeatSessionPerisistenceService extends AbstractPersiste
 		return repository.findByUserName(userName);
 	}
 	
+	public CurrentHeartbeatSession readCurrentHeartbeatSession() {
+		
+		List<CurrentHeartbeatSession> currentHeartbeatSessions = readAllCurrentHeartbeatSessions();
+		
+		CurrentHeartbeatSession currentHeartbeatSession = null;
+		
+		if(currentHeartbeatSessions.size() == 1) {
+			currentHeartbeatSession = currentHeartbeatSessions.get(0);
+		}else {
+			if(currentHeartbeatSessions.size() > 1) {
+				deleteAll();
+				logger.error("Found more than one session waiting for values. Delete every leftover session.");
+			}
+		}
+		
+		return currentHeartbeatSession;
+	}
+	
 	public CurrentHeartbeatSession storeCurrentHeartbeatSession(CurrentHeartbeatSession currentHeartbeatSession) {
 		return repository.save(currentHeartbeatSession);
+	}
+	
+	public List<CurrentHeartbeatSession> readAllCurrentHeartbeatSessions(){
+		return repository.findAll();
 	}
 	
 }
