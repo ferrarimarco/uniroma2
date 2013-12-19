918e7917054e57690c5cf7f2cb2946afca353927
Marco Ferrari <ferrari.marco@gmail.com>
2013-12-19 17:46:19 +0100

    HeartMonitorConstant: removed GENERIC_USERNAME constant, not needed

--- src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/HeartMonitorConstant.java
+++ src/main/java/info/ferrarimarco/uniroma2/sii/heartmonitor/HeartMonitorConstant.java
@@ -2,15 +2,14 @@ package info.ferrarimarco.uniroma2.sii.heartmonitor;
 
 
 public enum HeartMonitorConstant {
-	GENERIC_USERNAME("genericUser");
 	
-	private String value;
-	
-	private HeartMonitorConstant(String value) {
-		this.value = value;
-	}
-	
-	public String getStringValue() {
-		return value;
-	}
+//	private String value;
+//	
+//	private HeartMonitorConstant(String value) {
+//		this.value = value;
+//	}
+//	
+//	public String getStringValue() {
+//		return value;
+//	}
 }
