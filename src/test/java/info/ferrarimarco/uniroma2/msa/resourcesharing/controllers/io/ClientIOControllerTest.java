763673d012832f5cb57fd109ac869e3ed2facc85
Marco Ferrari <ferrari.marco@gmail.com>
2014-01-20 14:02:40 +0100

    - Added Jackson Mapper dependency - Removed some unneeded resources - Made ClientIOControllerTest work

--- src/test/java/info/ferrarimarco/uniroma2/msa/resourcesharing/controllers/io/ClientIOControllerTest.java
+++ src/test/java/info/ferrarimarco/uniroma2/msa/resourcesharing/controllers/io/ClientIOControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.Assert;
@@ -32,8 +33,7 @@ public class ClientIOControllerTest extends AbstractTestNGSpringContextTests {
 
 	private static Logger logger = LoggerFactory.getLogger(ClientIOControllerTest.class);
 	
-	private static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	private static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
 	private MockMvc mockMvc;
 
@@ -70,7 +70,7 @@ public class ClientIOControllerTest extends AbstractTestNGSpringContextTests {
 		
 		String email = PersistenceLayerInitializationService.USER_NAME_PREFIX + "0" + PersistenceLayerInitializationService.EMAIL_SUFFIX;
 		
-		MvcResult mvcResult = mockMvc.perform(get("/io/client/user/" + email))
+		MvcResult mvcResult = mockMvc.perform(get("/client/user/" + email))
 		.andExpect(status().isOk())
 		.andExpect(content().contentType(APPLICATION_JSON_UTF8))
 		.andExpect(jsonPath("$", hasSize(1))).andReturn();
