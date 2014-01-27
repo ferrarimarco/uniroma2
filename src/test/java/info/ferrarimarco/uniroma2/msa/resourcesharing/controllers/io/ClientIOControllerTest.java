e085f7a808f894ff1bc7edb508ed0de0a712f544
Marco Ferrari <ferrari.marco@gmail.com>
2014-01-27 19:35:06 +0100

    Organized imports.

--- src/test/java/info/ferrarimarco/uniroma2/msa/resourcesharing/controllers/io/ClientIOControllerTest.java
+++ src/test/java/info/ferrarimarco/uniroma2/msa/resourcesharing/controllers/io/ClientIOControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.Assert;
