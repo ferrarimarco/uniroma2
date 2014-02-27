ed477517e216440d599b248b7f81d806b746378c
Marco Ferrari <ferrari.marco@gmail.com>
2014-02-27 11:19:18 +0100

    Working on Roboelectric integration

---
+++ app/src/instrumentTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/FirstTest.java
@@ -0,0 +1,19 @@
+package info.ferrarimarco.uniroma2.msa.resourcesharing.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Marco on 26/02/14.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class FirstTest {
+
+    @Test
+    public void shouldFail() {
+        assertTrue(false);
+    }
+}
