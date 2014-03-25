c0a3550bb2c7680a510794fd7cf8898a7a52b935
Marco Ferrari <ferrari.marco@gmail.com>
2014-03-25 16:05:28 +0100

    Added interface for ResourceSharing REST service provider with Retrofit annotations.

---
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/services/ResourceSharingService.java
@@ -0,0 +1,15 @@
+package info.ferrarimarco.uniroma2.msa.resourcesharing.app.services;
+
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.Resource;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.User;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+public interface ResourceSharingService {
+
+    @GET(value="/client/resource/{resourceId}")
+    Resource getResourceById(@Path("resourceId") String id);
+
+    @GET(value="/client/user/{userName}")
+    public User getUserDetails(@Path("userName") String userName);
+}
