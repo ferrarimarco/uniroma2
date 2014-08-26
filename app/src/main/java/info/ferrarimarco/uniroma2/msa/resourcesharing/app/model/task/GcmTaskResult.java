29073494377b8525cb2a1cdca5fc5b06106b3657
Marco Ferrari <ferrari.marco@gmail.com>
2014-08-26 16:47:07 +0200

    First implementation of GcmTaskResult

---
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/model/task/GcmTaskResult.java
@@ -0,0 +1,8 @@
+package info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.task;
+
+public class GcmTaskResult extends AbstractTaskResult {
+
+    public GcmTaskResult() {
+        super();
+    }
+}
