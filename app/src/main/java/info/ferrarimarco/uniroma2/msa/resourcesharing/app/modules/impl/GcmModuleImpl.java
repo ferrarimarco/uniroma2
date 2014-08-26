bb3c47c9ed4b9e7cc02c688f0130a5da6131de56
Marco Ferrari <ferrari.marco@gmail.com>
2014-08-26 16:49:38 +0200

    Implemented GcmModuleImpl.
    This module provides GCM related components.

---
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/modules/impl/GcmModuleImpl.java
@@ -0,0 +1,18 @@
+package info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.impl;
+
+import android.content.Context;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import dagger.Module;
+import dagger.Provides;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.gcm.services.GcmMessagingServiceImpl;
+
+@Module(injects = {GcmMessagingServiceImpl.class}, complete = false, library = true)
+public class GcmModuleImpl {
+
+    @Provides
+    public GoogleCloudMessaging provideGoogleCloudMessaging(Context context) {
+        return GoogleCloudMessaging.getInstance(context);
+    }
+}
