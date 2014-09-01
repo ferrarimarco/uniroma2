f025f59f95b9dd5a95993c9fb2e275d74163a7b8
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-01 11:10:22 +0200

    Simplified according to a LINT warning

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/activities/InitActivity.java
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/activities/InitActivity.java
@@ -70,10 +70,7 @@ public class InitActivity extends AbstractAsyncTaskActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     private void checkRegisteredUser() {
