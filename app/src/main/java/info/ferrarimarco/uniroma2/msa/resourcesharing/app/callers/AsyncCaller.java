9712791b640de752ddc4a72feb2eea63a068de1b
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-03 15:53:04 +0200

    Removed AsyncCaller as it's not needed anymore

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/callers/AsyncCaller.java
+++
@@ -1,7 +0,0 @@
-package info.ferrarimarco.uniroma2.msa.resourcesharing.app.callers;
-
-public interface AsyncCaller {
-	void onBackgroundTaskCompleted(Object result);
-
-	void onBackgroundTaskCancelled(Object cancelledTask);
-}
