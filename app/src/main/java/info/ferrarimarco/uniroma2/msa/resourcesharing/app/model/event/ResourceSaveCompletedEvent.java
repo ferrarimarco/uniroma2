7cab451096bcedeab70e7cfaca65336d962a5c70
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-10 20:12:48 +0200

    Removed an unneeded event.

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/model/event/ResourceSaveCompletedEvent.java
+++
@@ -1,9 +0,0 @@
-package info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.event;
-
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.task.ResourceTaskResult;
-
-public class ResourceSaveCompletedEvent extends AbstractResourceEvent {
-    public ResourceSaveCompletedEvent(ResourceTaskResult result) {
-        super(result);
-    }
-}
