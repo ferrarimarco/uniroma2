53d0c0d0511615b30210c41447f2e116c1dd8233
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-13 16:05:03 +0200

    Removed an unneeded method for object graph module configuration

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/util/ObjectGraphUtils.java
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/util/ObjectGraphUtils.java
@@ -2,40 +2,28 @@ package info.ferrarimarco.uniroma2.msa.resourcesharing.app.util;
 
 import android.content.Context;
 
-import dagger.Module;
 import dagger.ObjectGraph;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.impl.ContextModuleImpl;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.impl.DaoModuleImpl;
 
-public class ObjectGraphUtils {
+public class ObjectGraphUtils{
 
     private static ObjectGraph objectGraph;
 
-    private ObjectGraphUtils() {
+    private ObjectGraphUtils(){
     }
 
-    private static void initObjectGraph(Context applicationContext) {
-        if (objectGraph != null) {
+    private static void initObjectGraph(Context applicationContext){
+        if(objectGraph != null){
             objectGraph = ObjectGraph.create(new ContextModuleImpl(applicationContext), new DaoModuleImpl());
         }
     }
 
-    public static ObjectGraph getObjectGraph(Context applicationContext) {
-        if (objectGraph == null) {
+    public static ObjectGraph getObjectGraph(Context applicationContext){
+        if(objectGraph == null){
             initObjectGraph(applicationContext);
         }
 
         return objectGraph;
     }
-
-    public static ObjectGraph addModulesToGraph(Context applicationContext, Module... modules) {
-        if (objectGraph == null) {
-            initObjectGraph(applicationContext);
-        }
-
-        objectGraph = objectGraph.plus(modules);
-
-        return objectGraph;
-    }
-
 }
