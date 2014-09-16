29636012ea0e61b61d6d9d7c43f2fcc1a118e093
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-16 17:05:41 +0200

    Implemented notification when receiving a new resource. The ShowResourcesActivity must implement an action listener for this.

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/services/intent/ResourceIntentService.java
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/services/intent/ResourceIntentService.java
@@ -1,12 +1,20 @@
 package info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
 
 import javax.inject.Inject;
 
 import dagger.ObjectGraph;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.R;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.activities.ShowResourcesActivity;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.Resource;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.task.ResourceTaskResult;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.task.ResourceTaskType;
@@ -23,10 +31,12 @@ import info.ferrarimarco.uniroma2.msa.resourcesharing.app.util.ObjectGraphUtils;
  */
 public class ResourceIntentService extends IntentService{
 
-    private static final String ACTION_SAVE_RESOURCE_FROM_ME = "info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.action.SAVE_RESOURCE_FROM_ME";
-    private static final String ACTION_RECEIVE_RESOURCE_FROM_OTHERS = "info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.action.SAVE_RESOURCE_FROM_ME";
+    private static final int NOTIFICATION_ID = 1;
 
-    private static final String EXTRA_PARAM_RESOURCE = "info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.extra.RESOURCE";
+    private static final String ACTION_SAVE_RESOURCE_FROM_ME = "info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.action.SAVE_RESOURCE_FROM_ME";
+    private static final String ACTION_RECEIVE_RESOURCE_FROM_OTHERS = "info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.action.RECEIVE_RESOURCE_FROM_OTHERS";
+
+    public static final String EXTRA_PARAM_RESOURCE = "info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.extra.RESOURCE";
 
     @Inject
     ResourceService resourceService;
@@ -112,6 +122,35 @@ public class ResourceIntentService extends IntentService{
             throw new RuntimeException("Unable to save the resource into local storage");
         }
 
-        resourceService.readResourcesFromLocalStorage(ResourceTaskType.READ_NEW_RESOURCES_LOCAL);
+        showNewResourceNotification(resource);
+    }
+
+    private void showNewResourceNotification(Resource resource) {
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(this, ShowResourcesActivity.class).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(EXTRA_PARAM_RESOURCE, resource);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack
+        stackBuilder.addParentStack(ShowResourcesActivity.class);
+        // Adds the Intent to the top of the stack
+        stackBuilder.addNextIntent(intent);
+
+        // Gets a PendingIntent containing the entire back stack
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder mBuilder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_action_event)
+                .setContentTitle(resource.getTitle())
+                .setStyle(new Notification.BigTextStyle().bigText(resource.getDescription()))
+                .setContentText(resource.getTitle())
+                .setTicker(getResources().getString(R.string.new_resource_notification_title))
+                .setAutoCancel(true)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setContentIntent(contentIntent);
+
+        notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
