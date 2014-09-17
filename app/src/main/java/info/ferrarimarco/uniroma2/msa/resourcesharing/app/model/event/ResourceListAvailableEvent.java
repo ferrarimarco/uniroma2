7927f01f98d16a36752687de463e292533e9b187
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-17 18:23:41 +0200

    Simplified event hierarchy.

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/model/event/ResourceListAvailableEvent.java
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/model/event/ResourceListAvailableEvent.java
@@ -2,8 +2,15 @@ package info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.event;
 
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.task.ResourceTaskResult;
 
-public class ResourceListAvailableEvent extends AbstractResourceEvent {
-    public ResourceListAvailableEvent(ResourceTaskResult result) {
-        super(result);
+public class ResourceListAvailableEvent{
+
+    private ResourceTaskResult result;
+
+    public ResourceListAvailableEvent(ResourceTaskResult result){
+        this.result = result;
+    }
+
+    public ResourceTaskResult getResult(){
+        return result;
     }
 }
