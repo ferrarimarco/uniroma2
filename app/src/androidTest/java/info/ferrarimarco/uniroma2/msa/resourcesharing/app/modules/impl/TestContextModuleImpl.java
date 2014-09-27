5b1c3da3722fe14614267b298756fd42cc7e2c84
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-27 19:16:35 +0200

    Updated test module to inject tests. Removed unneeded @Provide method.

--- app/src/androidTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/modules/impl/TestContextModuleImpl.java
+++ app/src/androidTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/modules/impl/TestContextModuleImpl.java
@@ -1,26 +1,11 @@
 package info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.impl;
 
-import android.content.Context;
-
-import javax.inject.Singleton;
-
 import dagger.Module;
-import dagger.Provides;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.GenericDao;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.GenericDaoTest;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.helper.DatabaseHelperManager;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.helper.DatabaseHelperManagerTest;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.Resource;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.DaoModule;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.impl.SharedPreferencesServiceImplTest;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.config.SharedPreferencesServiceImplTest;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.util.ObjectGraphUtilsTest;
 
-@Module(injects = {GenericDaoTest.class, DatabaseHelperManagerTest.class, SharedPreferencesServiceImplTest.class}, complete = false, overrides = true)
-public class TestContextModuleImpl implements DaoModule{
-
-    @Override
-    @Provides
-    @Singleton
-    public GenericDao<Resource> provideResourceDao(DatabaseHelperManager databaseHelperManager, Context context) {
-        return new GenericDao<>(databaseHelperManager, context);
-    }
+@Module(injects = {GenericDaoTest.class, DatabaseHelperManagerTest.class, SharedPreferencesServiceImplTest.class, ObjectGraphUtilsTest.class}, complete = false)
+public class TestContextModuleImpl{
 }
