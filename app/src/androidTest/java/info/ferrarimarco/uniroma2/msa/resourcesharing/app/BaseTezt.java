085c41fcc3e6f064234db800bad3f72250dca28f
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-27 19:17:20 +0200

    Switched to ObjectGraphUtils to build the object graph

--- app/src/androidTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/BaseTezt.java
+++ app/src/androidTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/BaseTezt.java
@@ -10,45 +10,46 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import dagger.ObjectGraph;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.impl.ContextModuleImpl;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.impl.TestContextModuleImpl;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.util.ObjectGraphUtils;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
-public abstract class BaseTezt {
+public abstract class BaseTezt{
 
     protected Context context;
 
     protected ObjectGraph objectGraph;
 
-    public BaseTezt() {
+    public BaseTezt(){
         context = Robolectric.application.getApplicationContext();
-        objectGraph = ObjectGraph.create(new ContextModuleImpl(context), new TestContextModuleImpl());
+        objectGraph = ObjectGraphUtils.getObjectGraph(context).plus(new TestContextModuleImpl());
+        singleDependencyCheck(objectGraph);
         objectGraph.inject(this);
     }
 
     @Test
-    public void contextTest() {
+    public void contextTest(){
         assertThat(context, notNullValue());
     }
 
     @Test
-    public void objectGraphTest() {
+    public void objectGraphTest(){
         assertThat(objectGraph, notNullValue());
     }
 
     @Test
     public abstract void dependencyInjectionTest();
 
-    protected void singleDependencyCheck(Object dependency) {
+    protected void singleDependencyCheck(Object dependency){
         assertThat(dependency, notNullValue());
     }
 
     @BeforeClass
-    public static void setup() {
+    public static void setup(){
         System.setProperty("robolectric.logging", "stdout");
     }
 }
