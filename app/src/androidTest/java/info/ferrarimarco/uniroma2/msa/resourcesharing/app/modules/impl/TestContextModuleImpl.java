b43dd77b62303167bde46ab6d28016d94274f265
Marco Ferrari <ferrari.marco@gmail.com>
2014-09-27 13:43:23 +0200

    Updated test architecture and modules to fix failed tests.

--- app/src/androidTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/modules/impl/TestContextModuleImpl.java
+++ app/src/androidTest/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/modules/impl/TestContextModuleImpl.java
@@ -6,17 +6,16 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.activities.CreateNewResourceActivity;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.activities.InitActivity;
-import info.ferrarimarco.uniroma2.msa.resourcesharing.app.activities.ShowResourcesActivity;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.GenericDao;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.GenericDaoTest;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.helper.DatabaseHelperManager;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.dao.helper.DatabaseHelperManagerTest;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.model.Resource;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.modules.DaoModule;
+import info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.impl.SharedPreferencesServiceImplTest;
 
-
-@Module(injects = {GenericDao.class, InitActivity.class, ShowResourcesActivity.class, CreateNewResourceActivity.class}, complete = false, library = true)
-public class DaoModuleImpl implements DaoModule {
+@Module(injects = {GenericDaoTest.class, DatabaseHelperManagerTest.class, SharedPreferencesServiceImplTest.class}, complete = false, overrides = true)
+public class TestContextModuleImpl implements DaoModule{
 
     @Override
     @Provides
