9860bdf00471c7536b5d8bfbfecb4fd1fe1d1151
Marco Ferrari <ferrari.marco@gmail.com>
2015-01-09 16:11:28 +0100

    Allow just minutes and hours as resource TTL (removed days)

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/activities/CreateNewResourceActivity.java
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/activities/CreateNewResourceActivity.java
@@ -24,7 +24,7 @@ import info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.intent.Resour
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.services.location.LocationService;
 import info.ferrarimarco.uniroma2.msa.resourcesharing.app.util.ObjectGraphUtils;
 
-public class CreateNewResourceActivity extends AbstractActivity implements AdapterView.OnItemSelectedListener{
+public class CreateNewResourceActivity extends AbstractActivity implements AdapterView.OnItemSelectedListener {
 
     @InjectView(R.id.resourceTitleEditText)
     EditText titleEditText;
@@ -45,7 +45,7 @@ public class CreateNewResourceActivity extends AbstractActivity implements Adapt
     LocationService locationService;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_create_new_resource);
         super.onCreate(savedInstanceState);
 
@@ -53,21 +53,21 @@ public class CreateNewResourceActivity extends AbstractActivity implements Adapt
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.create_new_resource, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if(id == R.id.action_save_new_resource){
-            if(areFieldsValid()){
+        if (id == R.id.action_save_new_resource) {
+            if (areFieldsValid()) {
                 String title = titleEditText.getText().toString();
                 String description = descriptionEditText.getText().toString();
                 String acquisitionMode = acquisitionModeEditText.getText().toString();
@@ -77,13 +77,11 @@ public class CreateNewResourceActivity extends AbstractActivity implements Adapt
                 // In secs
                 Long resourceTtl = ttlAmount;
 
-                // Convert to msec
-                if(selectedTtlUnit.equals(getResources().getString(R.string.minutes))){
+                // Convert to milliseconds
+                if (selectedTtlUnit.equals(getResources().getString(R.string.minutes))) {
                     resourceTtl *= 60 * 1000;
-                }else if(selectedTtlUnit.equals(getResources().getString(R.string.hours))){
+                } else if (selectedTtlUnit.equals(getResources().getString(R.string.hours))) {
                     resourceTtl *= 60 * 60 * 1000;
-                }else if(selectedTtlUnit.equals(getResources().getString(R.string.days))){
-                    resourceTtl *= 24 * 60 * 60 * 1000;
                 }
 
                 Location location = getLastKnownLocation();
@@ -100,23 +98,23 @@ public class CreateNewResourceActivity extends AbstractActivity implements Adapt
         return super.onOptionsItemSelected(item);
     }
 
-    private Boolean areFieldsValid(){
-        if(titleEditText.getText().toString().trim().isEmpty()){
+    private Boolean areFieldsValid() {
+        if (titleEditText.getText().toString().trim().isEmpty()) {
             Toast.makeText(this, getResources().getString(R.string.resource_save_error_empty_title), Toast.LENGTH_SHORT).show();
             return Boolean.FALSE;
         }
 
-        if(descriptionEditText.getText().toString().trim().isEmpty()){
+        if (descriptionEditText.getText().toString().trim().isEmpty()) {
             Toast.makeText(this, getResources().getString(R.string.resource_save_error_empty_description), Toast.LENGTH_SHORT).show();
             return Boolean.FALSE;
         }
 
-        if(acquisitionModeEditText.getText().toString().trim().isEmpty()){
+        if (acquisitionModeEditText.getText().toString().trim().isEmpty()) {
             Toast.makeText(this, getResources().getString(R.string.resource_save_error_empty_acquisition_mode), Toast.LENGTH_SHORT).show();
             return Boolean.FALSE;
         }
 
-        if(ttlAmountEditText.getText().toString().trim().isEmpty()){
+        if (ttlAmountEditText.getText().toString().trim().isEmpty()) {
             Toast.makeText(this, getResources().getString(R.string.resource_save_error_empty_ttl), Toast.LENGTH_SHORT).show();
             return Boolean.FALSE;
         }
@@ -124,19 +122,19 @@ public class CreateNewResourceActivity extends AbstractActivity implements Adapt
         return Boolean.TRUE;
     }
 
-    private Location getLastKnownLocation(){
+    private Location getLastKnownLocation() {
         return LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent){
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 
     @Override
-    public void onConnected(Bundle arg0){
+    public void onConnected(Bundle arg0) {
     }
 }
