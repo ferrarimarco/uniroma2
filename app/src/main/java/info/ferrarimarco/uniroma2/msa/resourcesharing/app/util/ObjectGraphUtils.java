5dd1c70cb2b6fc655bce33b9fbc8b10ad061dedf
Marco Ferrari <ferrari.marco@gmail.com>
2015-01-14 15:24:28 +0100

    Fixed a possible NPE when building Dagger Object Graph.

--- app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/util/ObjectGraphUtils.java
+++ app/src/main/java/info/ferrarimarco/uniroma2/msa/resourcesharing/app/util/ObjectGraphUtils.java
@@ -10,9 +10,21 @@ public class ObjectGraphUtils{
     private static ObjectGraph objectGraph;
 
     public static ObjectGraph getObjectGraph(Context context){
-        if(objectGraph == null && context != null){
-            objectGraph = ObjectGraph.create(new ContextModuleImpl(context.getApplicationContext()));
+
+        if (objectGraph == null) {
+            if (context != null) {
+                // Check if we are inside the application
+                if (context.getApplicationContext() != null) {
+                    objectGraph = ObjectGraph.create(new ContextModuleImpl(context.getApplicationContext()));
+                } else {
+                    // Do not cache this instance as we want to cache only when inside the application
+                    return ObjectGraph.create(new ContextModuleImpl(context));
+                }
+            } else {
+                throw new IllegalArgumentException("Context cannot be null");
+            }
         }
+
         return objectGraph;
     }
 }
