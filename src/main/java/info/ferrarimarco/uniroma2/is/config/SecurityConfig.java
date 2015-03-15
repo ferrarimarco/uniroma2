ff3bd852136b610e8c0f3b0026c61af6f893c095
Marco Ferrari <ferrari.marco@gmail.com>
2015-03-15 15:00:43 +0100

    Added login/logout configuration for spring security

--- src/main/java/info/ferrarimarco/uniroma2/is/config/SecurityConfig.java
+++ src/main/java/info/ferrarimarco/uniroma2/is/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package info.ferrarimarco.uniroma2.is.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,5 +14,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login.html")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
     }
 }
