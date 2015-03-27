9a900b87abe4a505372794232644bf929d176f5c
Marco Ferrari <ferrari.marco@gmail.com>
2015-03-27 17:03:16 +0100

    Completed implementation of new Products saving.

--- src/main/java/info/ferrarimarco/uniroma2/is/controller/EntitiesController.java
+++ src/main/java/info/ferrarimarco/uniroma2/is/controller/EntitiesController.java
@@ -1,5 +1,8 @@
 package info.ferrarimarco.uniroma2.is.controller;
 
+import info.ferrarimarco.uniroma2.is.model.Constants;
+import info.ferrarimarco.uniroma2.is.model.Product;
+import info.ferrarimarco.uniroma2.is.service.persistence.ClazzPersistenceService;
 import info.ferrarimarco.uniroma2.is.service.persistence.ProductPersistenceService;
 
 import org.apache.commons.lang.StringUtils;
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +20,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class EntitiesController {
     
     @Autowired
-    private ProductPersistenceService productService;
-
+    private ProductPersistenceService productPersistenceService;
+    
+    @Autowired
+    private ClazzPersistenceService clazzPersistenceService;
+    
     @RequestMapping(value = {"{entityName}", "/{entityName}"}, method = RequestMethod.GET)
     public String index(@PathVariable("entityName") String entityName, Model model, Pageable pageable) {
         if(StringUtils.isBlank(entityName)){
@@ -27,24 +34,28 @@ public class EntitiesController {
         String viewName = null;
         
         if("product".equals(entityName)){
-            model.addAttribute("allEntitiesPage", productService.findAll(pageable));
+            model.addAttribute("allEntitiesPage", productPersistenceService.findAll(pageable));
+            model.addAttribute("allClasses", clazzPersistenceService.findAll());
             viewName = "products.html";
         }else{
             throw new IllegalArgumentException("Entity name not valid"); 
         }
         
         model.addAttribute("entityName", entityName);
+        model.addAttribute(Constants.PRODUCT_MODEL_KEY, new Product());
         
         return viewName;
     }
     
     @RequestMapping(value = {"{entityName}", "/{entityName}"}, method = RequestMethod.POST)
-    public String createClass(@PathVariable("entityName") String entityName) {
-        return "products.html";
+    public String createEntity(@PathVariable("entityName") String entityName, Model model, Pageable pageable, @ModelAttribute Product product) {
+        Product savedProduct = productPersistenceService.save(product);
+        model.addAttribute(Constants.PRODUCT_MODEL_KEY, savedProduct);
+        return index(entityName, model, pageable);
     }
     
     @RequestMapping(value = {"{entityName}/{entityId}", "/{entityName}/{entityId}"}, method = RequestMethod.PUT)
-    public String updateClass(
+    public String updateEntity(
             @PathVariable("entityName") String entityName,
             @PathVariable("entityId") String entityId) {
         return "products.html";
