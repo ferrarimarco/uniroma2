59ae9d376d5e9cb59da3a17c2a5da5bd5a007d5f
Marco Ferrari <ferrari.marco@gmail.com>
2015-04-01 21:54:48 +0200

    Configured jackson mapper.

--- src/main/java/info/ferrarimarco/uniroma2/is/config/servlet/SpringMvcConfig.java
+++ src/main/java/info/ferrarimarco/uniroma2/is/config/servlet/SpringMvcConfig.java
@@ -6,10 +6,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebMvc
@@ -33,5 +40,25 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 
         argumentResolvers.add(pageableHandlerMethodArgumentResolver);
         super.addArgumentResolvers(argumentResolvers);
+    }
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jacksonMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+        jacksonMessageConverter.setObjectMapper(mapper);
+        jacksonMessageConverter.setPrettyPrint(true);
+        converters.add(jacksonMessageConverter);
+        super.configureMessageConverters(converters);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.TEXT_HTML);
+        configurer.favorPathExtension(true);
+        configurer.favorParameter(false);
+        configurer.useJaf(false).
+        mediaType("json", MediaType.APPLICATION_JSON);
     }
 }
