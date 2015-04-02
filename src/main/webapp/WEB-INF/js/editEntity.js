3ea5ef9ceb8884e0193a32ff791553be256e4498
Marco Ferrari <ferrari.marco@gmail.com>
2015-04-02 16:21:17 +0200

    Working on populating edit entity modal

--- src/main/webapp/WEB-INF/js/editEntity.js
+++ src/main/webapp/WEB-INF/js/editEntity.js
@@ -1,23 +1,17 @@
 $('#editEntityModal').on('show.bs.modal', function(event) {
 	var button = $(event.relatedTarget) // Button that triggered the modal
-	var entityName = button.data('entity-name')
-	var entityId = button.data('entity-id')
-	loadProduct(entityName, entityId)
+	$('#productName').val(button.data('entity-instance-name'))
+	$('#productBarcode').val(button.data('entity-barcode'))
+	$('#productBrand').val(button.data('entity-brand'))
+	$('#productAmount').val(button.data('entity-amount'))
+	
+	// Set entity class
+	$("#productClass").filter(function() {
+	    return this.text == button.data('entity-class'); 
+	}).attr('selected', true);
+	
+	// Append entity ID to form action
+	var action = $("editProductForm").attr('action')
+	action = action + "/" + button.data('entity-id')
+	$("#search-form").attr("action", action);
 });
-
-function loadProduct(entityName, entityId) {
-	$.ajax({
-		url: entityName + "/" + entityId,
-		dataType: 'json',
-		success: function(result){
-			$('#productName').val(result.name)
-			$("#productClass").val(result.clazz.id)
-			$('#productBarcode').val(result.barCode)
-			$('#productBrand').val(result.brand)
-			$('#productAmount').val(result.amount)
-		},
-		error: function(request, textStatus, errorThrown) {
-			alert(textStatus);
-		}
-	});
-}
