be63b07703138e3d9d36eb34dc0c78812c505436
Marco Ferrari <ferrari.marco@gmail.com>
2015-04-04 19:40:19 +0200

    Hide amount field when editing.

--- src/main/webapp/WEB-INF/js/editEntity.js
+++ src/main/webapp/WEB-INF/js/editEntity.js
@@ -7,6 +7,10 @@ $('#editEntityModal').on('show.bs.modal', function(event) {
 	$('#productAmount', modal).val(button.data('entity-amount'));
 	$('#productId', modal).val(button.data('entity-id'));
 	
+	// Hide amount field if editing
+	if(typeof button.data('entity-id') !== typeof undefined && button.data('entity-id') !== false)
+		$("#productAmountField", modal).hide();
+	
 	// Set entity class
 	$("#productClass option:contains(" + button.data('entity-clazz') + ")", modal).attr('selected', true);
 });
