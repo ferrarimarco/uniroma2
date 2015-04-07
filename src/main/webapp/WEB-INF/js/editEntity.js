be91f20ad1e8dcf72e7d58aa5bf72a515dc59633
Marco Ferrari <ferrari.marco@gmail.com>
2015-04-07 16:29:47 +0200

    Added form fields validation for edit entity

--- src/main/webapp/WEB-INF/js/editEntity.js
+++ src/main/webapp/WEB-INF/js/editEntity.js
@@ -4,8 +4,17 @@ $('#editEntityModal').on('show.bs.modal', function(event) {
 	$('#productName', modal).val(button.data('entity-title'));
 	$('#productBarcode', modal).val(button.data('entity-barcode'));
 	$('#productBrand', modal).val(button.data('entity-brand'));
+	
+	if(button.data('entity-brand') != undefined){
+		// Set entity class
+		$("#productClass option:contains(" + button.data('entity-clazz') + ")", modal).attr('selected', true);		
+	}else{
+		$('#productClass', modal).prepend("<option value=''></option>").val('');
+	}
+	
 	$('#productId', modal).val(button.data('entity-id'));
 	
-	// Set entity class
-	$("#productClass option:contains(" + button.data('entity-clazz') + ")", modal).attr('selected', true);
+	var legend = $("#editProductModalLegend", modal).text();
+	legend = legend.substr(0, legend.indexOf(': ') + 2);
+	$("#editProductModalLegend", modal).text(legend + button.data('entity-title'));
 });
