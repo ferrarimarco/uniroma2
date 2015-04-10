d7d02c7017e9d43beb618e633c47334afbee00b0
Marco Ferrari <ferrari.marco@gmail.com>
2015-04-10 19:52:13 +0200

    Added all products to model.

--- src/main/java/info/ferrarimarco/uniroma2/is/controller/StatsController.java
+++ src/main/java/info/ferrarimarco/uniroma2/is/controller/StatsController.java
@@ -1,18 +1,35 @@
 package info.ferrarimarco.uniroma2.is.controller;
 
+import java.util.List;
+
+import info.ferrarimarco.uniroma2.is.model.Product;
 import info.ferrarimarco.uniroma2.is.model.dto.StatsDto;
 
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/stats")
 public class StatsController extends AbstractController {
     
+    @ModelAttribute("allProducts")
+    private List<Product> getAllProducts(){
+        return productPersistenceService.findAll();
+    }
+    
     @RequestMapping
     public String index(Model model){
-        model.addAttribute(new StatsDto());
+        if(!model.containsAttribute(Conventions.getVariableName(StatsDto.class)))
+            model.addAttribute(new StatsDto());
+        return "stats.html";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String postStatRequest(Model model, @ModelAttribute StatsDto statsDto){
         return "stats.html";
     }
 }
