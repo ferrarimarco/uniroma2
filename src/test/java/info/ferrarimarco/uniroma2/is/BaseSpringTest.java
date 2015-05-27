29ae8a958471f5e4d3f1252d9b5b8d8d08faa021
Marco Ferrari <ferrari.marco@gmail.com>
2015-05-27 19:07:56 +0200

    Test environment setup

---
+++ src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringTest.java
@@ -0,0 +1,18 @@
+package info.ferrarimarco.uniroma2.is;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import info.ferrarimarco.uniroma2.is.config.context.RootConfig;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { RootConfig.class })
+public abstract class BaseSpringTest extends AbstractTestNGSpringContextTests {
+
+    @BeforeClass
+    protected void setup() throws Exception {
+        assertThat(applicationContext, notNullValue());
+    }
+}
