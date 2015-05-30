7932bf7eba8e7d4cc197506d02e7ee0b179f066f
Marco Ferrari <ferrari.marco@gmail.com>
2015-05-30 17:54:37 +0200

    Implemented unit tests for StatService.

--- src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringTest.java
+++ src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringTest.java
@@ -7,12 +7,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { RootConfig.class })
 public abstract class BaseSpringTest extends AbstractTestNGSpringContextTests {
 
     @BeforeClass
+    @Test(groups = {"checkintest" })
     protected void setup() throws Exception {
         assertThat(applicationContext, notNullValue());
     }
+    
 }
