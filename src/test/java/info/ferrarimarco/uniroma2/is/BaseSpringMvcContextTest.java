2e9fbbf7cf9f4df98a253b2bd8775987391ae120
Marco Ferrari <ferrari.marco@gmail.com>
2015-06-04 19:15:29 +0200

    Added some more flexiblity to spring test class hierarchy

--- src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringMvcContextTest.java
+++ src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringMvcContextTest.java
@@ -5,19 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
 @Slf4j
 public abstract class BaseSpringMvcContextTest extends BaseSpringMvcTest{
     @Autowired
     protected WebApplicationContext webApplicationContext;
     
+    @Override
     protected void setupMockMvc() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Override
-    protected void setupMockMvc(Object... controllers) {
+    protected void setupMockMvc(Object[] controllers) {
         log.warn("This class does not support standalone setup for controllers. It will initialize the whole context instead");
         setupMockMvc();
+    }
+    
+    @Override
+    protected void setupMockMvc(Object[] controllers, HandlerMethodArgumentResolver[] argumentResolvers){
+        setupMockMvc(null);
     }
 }
--- src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringMvcTest.java
+++ src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringMvcTest.java
@@ -4,16 +4,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.testng.annotations.BeforeClass;
 
 public abstract class BaseSpringMvcTest extends BaseSpringTest{
     protected MockMvc mockMvc;
     
     protected abstract void setupMockMvc();
-    protected abstract void setupMockMvc(Object... controllers);
+    protected abstract void setupMockMvc(Object[] controllers);
+    protected abstract void setupMockMvc(Object[] controllers, HandlerMethodArgumentResolver[] argumentResolvers);
     
-    @BeforeClass(groups = {"springMvcTestInit"}, dependsOnGroups = {"springBaseTestInit"})
-    protected void setup(){
+    @BeforeClass(groups = {"springMvcTestInit"}, dependsOnGroups = {"springUTinit"})
+    protected void mockMvcInitTest(){
         assertThat(mockMvc, notNullValue());
     }
 }
