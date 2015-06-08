2ec0c27fb42944827f1b01ece80232b45dea566b
Marco Ferrari <ferrari.marco@gmail.com>
2015-06-08 13:28:21 +0200

    Updated generic model attributes mocks.

--- src/test/java/info/ferrarimarco/uniroma2/is/controller/AbstractControllerUT.java
+++ src/test/java/info/ferrarimarco/uniroma2/is/controller/AbstractControllerUT.java
@@ -1,5 +1,6 @@
 package info.ferrarimarco.uniroma2.is.controller;
 
+import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -8,6 +9,7 @@ import info.ferrarimarco.uniroma2.is.config.context.RootConfig;
 import info.ferrarimarco.uniroma2.is.config.servlet.SpringMvcConfig;
 import info.ferrarimarco.uniroma2.is.model.Category;
 import info.ferrarimarco.uniroma2.is.model.Clazz;
+import info.ferrarimarco.uniroma2.is.model.Product;
 import info.ferrarimarco.uniroma2.is.service.persistence.CategoryPersistenceService;
 import info.ferrarimarco.uniroma2.is.service.persistence.ClazzPersistenceService;
 import info.ferrarimarco.uniroma2.is.service.persistence.ProductInstancePersistenceService;
@@ -19,6 +21,8 @@ import java.util.List;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.context.ContextConfiguration;
@@ -42,6 +46,8 @@ public abstract class AbstractControllerUT extends BaseSpringMvcSingleController
     
     protected Category category;
     protected Clazz clazz;
+    protected Product product;
+    protected Page<Product> productsPage;
     
     protected abstract AbstractController getController();
     
@@ -58,17 +64,33 @@ public abstract class AbstractControllerUT extends BaseSpringMvcSingleController
     protected void setupMocksForGenericModelAttributes(){
         category = new Category();
         category.setId("cat-id");
+        category.setName("cat-name");
+        when(categoryPersistenceService.findById(category.getId())).thenReturn(category);
+        
         clazz = new Clazz();
         clazz.setId("clazz-id");
         clazz.setCategory(category);
+        clazz.setName("clazz-name");
+        when(clazzPersistenceService.findById(clazz.getId())).thenReturn(clazz);
         
         List<Category> categories = new ArrayList<>();
         categories.add(category);
+        when(categoryPersistenceService.findAll()).thenReturn(categories);
+        
         List<Clazz> clazzes = new ArrayList<>();
         clazzes.add(clazz);
-        
-        when(categoryPersistenceService.findAll()).thenReturn(categories);
         when(clazzPersistenceService.findAll()).thenReturn(clazzes);
+        
+        product = Product.builder().category(category).clazz(clazz).brand("brand").build();
+        product.setId("prod1-id");
+        product.setName("product-name");
+        when(productPersistenceService.findById(product.getId())).thenReturn(product);
+
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+        productsPage = new PageImpl<Product>(products);
+        
+        when(productPersistenceService.findAll(notNull(PageRequest.class))).thenReturn(productsPage);
     }
     
     @AfterMethod(dependsOnGroups = "genericModelAttributesNeeded")
