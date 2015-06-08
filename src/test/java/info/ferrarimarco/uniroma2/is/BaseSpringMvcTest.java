41e9f3495dffd6019341adad679e663bded5ed54
Marco Ferrari <ferrari.marco@gmail.com>
2015-06-08 13:27:47 +0200

    Configured json-path to use Jackson

--- src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringMvcTest.java
+++ src/test/java/info/ferrarimarco/uniroma2/is/BaseSpringMvcTest.java
@@ -3,9 +3,19 @@ package info.ferrarimarco.uniroma2.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.testng.annotations.BeforeClass;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
 
 public abstract class BaseSpringMvcTest extends BaseSpringTest{
     protected MockMvc mockMvc;
@@ -17,5 +27,26 @@ public abstract class BaseSpringMvcTest extends BaseSpringTest{
     @BeforeClass(groups = {"springMvcTestInit"}, dependsOnGroups = {"springUTinit"})
     protected void mockMvcInitTest(){
         assertThat(mockMvc, notNullValue());
+        
+        // Configure json-path
+        Configuration.setDefaults(new Configuration.Defaults() {
+            private final JsonProvider jsonProvider = new JacksonJsonProvider();
+            private final MappingProvider mappingProvider = new JacksonMappingProvider();
+
+            @Override
+            public JsonProvider jsonProvider() {
+                return jsonProvider;
+            }
+
+            @Override
+            public MappingProvider mappingProvider() {
+                return mappingProvider;
+            }
+
+            @Override
+            public Set<Option> options() {
+                return EnumSet.noneOf(Option.class);
+            }
+        });
     }
 }
