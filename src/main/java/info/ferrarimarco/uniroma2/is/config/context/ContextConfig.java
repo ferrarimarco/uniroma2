a28b926cc1e8a55975db5c985c6eceb168014771
Marco Ferrari <ferrari.marco@gmail.com>
2015-06-28 17:03:59 +0200

    Enable cache support

--- src/main/java/info/ferrarimarco/uniroma2/is/config/context/ContextConfig.java
+++ src/main/java/info/ferrarimarco/uniroma2/is/config/context/ContextConfig.java
@@ -1,5 +1,6 @@
 package info.ferrarimarco.uniroma2.is.config.context;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -7,5 +8,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan({"info.ferrarimarco.uniroma2.is.service", "info.ferrarimarco.uniroma2.is.config.initializer.persistence"})
+@EnableCaching
 public class ContextConfig {
 }
